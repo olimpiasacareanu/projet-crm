d0ad7619e1d3d1c731238f5b45160a2ebb05cbee
hello-world <olimpiasacareanu@yahoo.com>
2021-04-05 11:17:31 +0200

    EDIT PROFIL / PASSWORD / NAME

---
+++ public/js/main.js
@@ -0,0 +1,18 @@
+/*
+$('.details-contact img').on({
+    'click': function(){
+        $('#zoom').css("display", "block");
+
+        var image = $(this).children("#project").attr("src");
+        console.log(image);
+        $("#img01").attr("src", image);
+    }
+});
+
+$('span').on({
+    'click': function() {
+        $('#zoom').css("display", "none");
+    }
+})
+
+ */
